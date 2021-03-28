b5ee43c495162e27fc508ec107a85425c69666b5
MontyPan <monty.pan@dontcareabout.us>
2021-03-28 14:44:44 +0800

    增加：箱子 VO

---
+++ src/main/java/us/dontcareabout/kkfan/shared/vo/Crate.java
@@ -0,0 +1,151 @@
+package us.dontcareabout.kkfan.shared.vo;
+
+import java.util.Date;
+
+/**
+ * 「箱子」的對應 VO。
+ * <p>
+ * 若 {@link #getDeleteTime()} 不為 null 表示該筆資料已經刪除。
+ */
+public class Crate {
+	private long id;
+
+	private Date createTime;
+	private Date deleteTime;
+
+	private String category;
+	private int number;
+
+	//spec 區
+	private double height;
+	private double length;
+	private double width;
+	private String color;
+	////
+
+	//製造資訊區
+	private int mfYear;
+	private String mfr;
+	////
+
+	//管理區
+	private Location location;
+	private String item;
+	////
+
+	private String note;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getDeleteTime() {
+		return deleteTime;
+	}
+
+	public void setDeleteTime(Date deleteTime) {
+		this.deleteTime = deleteTime;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/** @return 製造年份 */
+	public int getMfYear() {
+		return mfYear;
+	}
+
+	public void setMfYear(int mfYear) {
+		this.mfYear = mfYear;
+	}
+
+	/** @return 製造商 */
+	public String getMfr() {
+		return mfr;
+	}
+
+	public void setMfr(String mfr) {
+		this.mfr = mfr;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+}
