35fc4fe90ea6835c34eda873352068dcf3944bb9
MontyPan <monty.pan@dontcareabout.us>
2021-04-03 00:45:10 +0800

    增加 field：floor、polygon

--- src/main/java/us/dontcareabout/kkfan/shared/vo/Location.java
+++ src/main/java/us/dontcareabout/kkfan/shared/vo/Location.java
@@ -1,11 +1,18 @@
 package us.dontcareabout.kkfan.shared.vo;
 
+/**
+ * 若 {@link #getType()} 為 {@value LocationType#trip} 或 {@value LocationType#unborn}，
+ * 則 {@link #getFloor()} 與 {@link #getPolygon()} 的值無意義。
+ */
 public class Location {
 	private long id;
 
 	private String name;
 
 	private LocationType type;
+
+	private int floor;
+	private String polygon;
 
 	public long getId() {
 		return id;
@@ -29,5 +36,21 @@ public class Location {
 
 	public void setType(LocationType type) {
 		this.type = type;
+	}
+
+	public int getFloor() {
+		return floor;
+	}
+
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+	public String getPolygon() {
+		return polygon;
+	}
+
+	public void setPolygon(String polygon) {
+		this.polygon = polygon;
 	}
 }
