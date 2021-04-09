3dd4fc2051c1e1d46ceb1b65115669ea24fdc31c
MontyPan <monty.pan@dontcareabout.us>
2021-04-10 01:20:50 +0800

    增加：Crate 的 serial 字串組成 method

--- src/main/java/us/dontcareabout/kkfan/client/util/StringUtil.java
+++ src/main/java/us/dontcareabout/kkfan/client/util/StringUtil.java
@@ -1,7 +1,13 @@
 package us.dontcareabout.kkfan.client.util;
 
+import us.dontcareabout.kkfan.shared.vo.Crate;
+
 public class StringUtil {
 	public static String floor(int floor) {
 		return floor > 0 ? floor + "F" : "B" + Math.abs(floor);
+	}
+
+	public static String serial(Crate crate) {
+		return crate.getCategory() + "-" + crate.getNumber();
 	}
 }
