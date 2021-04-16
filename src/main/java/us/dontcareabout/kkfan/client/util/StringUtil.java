46082ef3040f71a1a1a206bcbb2478147dc239ef
MontyPan <monty.pan@dontcareabout.us>
2021-04-16 17:12:11 +0800

    增加：size() 以及 LocationType 的 toString()

--- src/main/java/us/dontcareabout/kkfan/client/util/StringUtil.java
+++ src/main/java/us/dontcareabout/kkfan/client/util/StringUtil.java
@@ -1,6 +1,7 @@
 package us.dontcareabout.kkfan.client.util;
 
 import us.dontcareabout.kkfan.shared.vo.Crate;
+import us.dontcareabout.kkfan.shared.vo.LocationType;
 
 public class StringUtil {
 	public static String floor(int floor) {
@@ -9,5 +10,20 @@ public class StringUtil {
 
 	public static String serial(Crate crate) {
 		return crate.getCategory() + "-" + crate.getNumber();
+	}
+
+	public static String size(Crate crate) {
+		return crate.getLength() + " x " + crate.getWidth() + " x " + crate.getHeight();
+	}
+
+	public static String toString(LocationType type) {
+		switch(type) {
+		case gallery: return "展廳";
+		case somewhere: return "館內";
+		case storage: return "庫房";
+		case trip: return "外出中";
+		case unborn:
+		default: return "尚未入帳";
+		}
 	}
 }
