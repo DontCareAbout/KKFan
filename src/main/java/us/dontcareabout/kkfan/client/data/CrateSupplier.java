5c539bb206f58e138a7300998378502ff813238e
MontyPan <monty.pan@dontcareabout.us>
2021-05-18 00:35:00 +0800

    改用 REST 的真實資料，並在準備好 Crate.setLocation()

--- src/main/java/us/dontcareabout/kkfan/client/data/CrateSupplier.java
+++ src/main/java/us/dontcareabout/kkfan/client/data/CrateSupplier.java
@@ -7,10 +7,15 @@ import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.GwtEvent.Type;
 
 import us.dontcareabout.kkfan.client.data.CrateReadyEvent.CrateReadyHandler;
+import us.dontcareabout.kkfan.client.data.gf.Callback;
+import us.dontcareabout.kkfan.client.data.gf.Logistics;
 import us.dontcareabout.kkfan.client.data.gf.Supplier;
-import us.dontcareabout.kkfan.client.util.Mocker;
 import us.dontcareabout.kkfan.shared.vo.Crate;
+import us.dontcareabout.kkfan.shared.vo.Location;
 
+/**
+ * {@link Logistics} 的 {@link Location} 資料必須在 {@link #fetch()} 之前 ready。 
+ */
 public class CrateSupplier extends Supplier<List<Crate>, CrateReadyHandler> {
 
 	@Override
@@ -30,12 +35,31 @@ public class CrateSupplier extends Supplier<List<Crate>, CrateReadyHandler> {
 
 	@Override
 	public void fetch() {
-		ready(Mocker.crateList());
+		DataCenter.crateRester.getList(new Callback<List<Crate>>() {
+			@Override
+			public void onSuccess(List<Crate> data) {
+				List<Location> locList = Logistics.getData("location");
+
+				for (Crate c : data) {
+					for (Location loc : locList) {
+						if (c.getLocationId() == loc.getId()) {
+							c.setLocation(loc);
+							break;
+						}
+					}
+				}
+
+				ready(data);
+			}
+
+			@Override
+			public void onError(Throwable exception) {
+			}
+		});
 	}
 
 	@Override
 	public Date nextExpire() {
 		return latterDate(60);
 	}
-
 }
